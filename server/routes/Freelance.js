2b35cd07a43c25e8bb240d271136d4678ea1769b
khaled <ksmesdev@gmail.com>
2023-02-18 09:00:19 +0200

    freelance v1.1

--- server/routes/Freelance.js
+++ server/routes/Freelance.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const path = require('path')
 
+
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "../../client/dist/freeLance.html"))
+    // res.sendFile(path.join(__dirname, "../../client/dist/freeLance.html"))
+    console.log(__dirname)
+    res.send('test')
 })
 
 module.exports = router;
