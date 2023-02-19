c302dd8faa15a23928f6da6caf4f242fdd98d685
khaled <ksmesdev@gmail.com>
2023-02-19 11:34:32 +0200

    freelance v1.1

--- server/routes/Freelance.js
+++ server/routes/Freelance.js
@@ -4,26 +4,17 @@ const path = require('path')
 const fs = require('fs')
 
 
-const mypath = path.join(__dirname, "../../client")
+// const mypath = path.join(__dirname, "../../client")
 const mypath2 = path.join(__dirname, "../../client/dist")
 
 router.get('/', (req, res) => {
-    fs.readdir(mypath, (err, files) => {
-        if(err){
-            console.log(err)
-        } else {
-            files.forEach(file => {
-                console.log(file)
-            })
-        }
-    })
-    console.log('path2')
     fs.readdir(mypath2, (err, files) => {
         if(err){
-            console.log(err)
+            // console.log(err)
         } else {
             files.forEach(file => {
-                console.log(file)
+                const filePath = path.join(mypath2, file);
+                console.log(filePath);
             })
         }
     })
