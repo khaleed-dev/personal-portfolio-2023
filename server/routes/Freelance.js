2b5fe8c0b3ce80c3ba9fd024b6452c5456cd06f9
khaled <ksmesdev@gmail.com>
2023-02-19 11:53:54 +0200

    freelance v1.1

--- server/routes/Freelance.js
+++ server/routes/Freelance.js
@@ -3,22 +3,23 @@ const router = express.Router();
 const path = require('path')
 const fs = require('fs')
 
-
-// const mypath = path.join(__dirname, "../../client")
-const mypath2 = path.join(__dirname, "../../client/dist")
-
 router.get('/', (req, res) => {
-    fs.readdir(mypath2, (err, files) => {
-        if(err){
-            // console.log(err)
-        } else {
-            files.forEach(file => {
-                const filePath = path.join(mypath2, file);
-                console.log(filePath);
-            })
-        }
-    })
-    res.sendFile(path.join(__dirname, "../../client/dist/freeLance.html"))
+    let filePath = '/var/task/client/dist/freelance.html';
+    fs.access(filePath, fs.constants.F_OK, (err) => {
+      if (!err) {
+        res.sendFile(filePath);
+      } else {
+        filePath = path.join(__dirname, "../../client/dist/freeLance.html");
+        fs.access(filePath, fs.constants.F_OK, (err) => {
+          if (!err) {
+            res.sendFile(filePath);
+          } else {
+            res.send("filenotfound");
+          }
+        });
+      }
+    });
+    
 })
 
 module.exports = router;
